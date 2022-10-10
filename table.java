3126c607dd103463e03d3748c73921f9de528698
uikrati <87569719+uikrati@users.noreply.github.com>
2022-10-10 22:18:06 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class table {
+    public static void main(String[] args) {
+        class Solution {
+            static ArrayList<Integer> getTable(int N) {
+                // code here
+                ArrayList<Integer> table = new ArrayList<Integer>();
+                for (int i = 1; i <= 10; i++) {
+                    table.add(N * i);
+                }
+                return table;
+
+            }
+        }
+    }
+}
